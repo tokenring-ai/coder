e02bcba76beb065247671aa3480bf0bb79e9f33c
Mark Dierolf <mdierolf@cloudquote.io>
2025-08-16 17:19:16 -0700

    Install additional dependencies: python3 & build-essential in Docker setup script

--- docker/install.sh
+++ docker/install.sh
@@ -4,11 +4,14 @@ set -e
 
 
 apt update
-apt install -y git
-
-rm -rf /var/cache/apt
-rm -rf /var/lib/apt/lists/*
+apt install -y git python3 build-essential
 
 cd /repo
 rm -rf /repo/node_modules
 bun install
+
+rm -rf /var/cache/apt
+rm -rf /var/lib/apt/lists/*
+
+apt remove -y build-essential
+apt autoremove -y
